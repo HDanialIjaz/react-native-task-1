a9a4e4bdf9ba256fc3e99542de61cff97d6ef733
Hafiz Muhammad Danial <danialijaz098@gmail.com>
2024-11-06 22:51:19 -0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+const Show = () => {
+  return(
+  <View>
+  <Text> Hello World </Text>
+  </View>);
+};
+ 
+ export default Show;
